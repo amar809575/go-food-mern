912b69e242ae9f312ef066168eb55c990ac1cfd9
amar809575 <kumavatha@gmail.com>
2024-08-09 18:14:47 +0530

    Changes done

--- backend/index.js
+++ backend/index.js
@@ -16,6 +16,12 @@ mongoDB()
       next();
     });
 
+    app.use((req, res, next) => {
+      res.setHeader('Cache-Control', 'no-store');
+      next();
+    });
+
+    
     app.use(cors());
     app.use(express.json());
     app.use('/api/', require("./Routes/CreatUser"));
